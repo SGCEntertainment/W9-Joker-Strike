3e9728e8322d18de1dae0bd429ca822dc8bb562d
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-12-10 16:30:06 +0300

    update

---
+++ Assets/W9 Joker Strike/Scripts/Fruit.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    private Rigidbody2D Rigidbody { get; set; }
+
+    private void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        transform.position = new Vector2(0, -6.28f);
+
+        Rigidbody.AddForce(Vector2.up * 25, ForceMode2D.Impulse);
+        Rigidbody.angularVelocity = Random.Range(80, 180.0f);
+    }
+}
